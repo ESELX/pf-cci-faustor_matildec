c40d2fcd762dd086025e914e31da32b5c304efc7
faus2ribeiro <Faus2Ribeiro@users.noreply.github.com>
2020-01-31 21:17:23 +0000

    Bolas

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,20 +1,56 @@
+let circles = [];
+
 function setup(){
 
   createCanvas(windowWidth,windowHeight);
-
+  for (let i = 0; i < 100; i++) {
+    let x = random(width);
+    let y = random(50, height);
+    let d = 50;
+    circles[i] = new circle (x,y,d);
+  }
 
 }
 
 
 function draw() {
-  background(240);
-  strokeWeight(2);
+  background(50);
 
-  line(width/2, height/2, mouseX, mouseY);
-  line(width/2, height/2,mouseX,height/2);
-  line(mouseX, mouseY,mouseX,height/2);
+  for (let circle of circles) {
+    circle.show();
+  }
 
-  textSize(width / 50);
-  text(dist(width/2, height/2, mouseX, mouseY),50,50);
+  for (let i = 0; i < circles.length; i++) {
 
+    circles[i].show();
+  }
+}
+
+class circle {
+  constructor(x, y, d) {
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    this.brightness = 0;
+  }
+
+  changeColor(bright) {
+      this.brightness = bright;
+    }
+
+    contains(px, py) {
+      let r = dist(px, py, this.x, this.y);
+      if (r < this.d) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+  show() {
+   stroke(255);
+   strokeWeight(4);
+   fill(this.brightness, 125);
+   ellipse(this.x, this.y, this.d);
+  }
 }
