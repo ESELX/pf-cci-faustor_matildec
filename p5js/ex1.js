71070abbfbb9ce36fb9cca0a498501403b27971c
MatildeCoelho <47894321+MatildeCoelho@users.noreply.github.com>
2020-02-07 16:39:36 +0000

    Revert "final"
    This reverts commit 4c8020b3a860d1ea17ebfa2022b07bf75e1d8ac7.

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,28 +1,32 @@
-//lista de variáveis
+//variáveis
 let circles = [];
-let primos = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97];
-let score = 75;
-let hp = false;
-let control = false;
-
+let score = 100;
 
 
 function setup(){
 
   createCanvas(windowWidth,windowHeight);
-
-
   let i = 1
   for (; i < 101; i++) {
-    let x = random(75,width-75);
-    let y = random(75,height-50);
-    let r = 25;
-    let c = new circle (x,y,r,i);
-    let otherx = random(75,width-75);
-    let othery = random(75,height-50);
+    let x = random(width);
+    let y = random(50, height);
+    let d = 50;
+    let c = new circle (x,y,d,i);
     circles.push(c);
+  }
+
+
+}
+
+function mousePressed() {
+  for (let i = circles.length - 1; i >= 0; i--) {
+    if (circles[i].contains(mouseX, mouseY)) {
+      circles.splice(i,1);
+      score = score-1;
+      }
 
   }
+
 }
 
 
@@ -32,11 +36,8 @@ function draw() {
   fill(250);
   textSize(20);
   textAlign(LEFT);
-  text('Retira os números compósitos', 10, 25);
+  text('Retira todos números que não são primos!', 10, 25);
   text(score, width-60 ,25);
-  textSize(10);
-  text('© Fausto Ribeiro e Matilde Coelho, 2020', width-200, height-10);
-
   for (let i = 0; i < circles.length; i++) {
     if (circles[i].contains(mouseX,mouseY)) {
       circles[i].changeColor(255);
@@ -44,54 +45,22 @@ function draw() {
       circles[i].changeColor(50);
     }
       circles[i].show();
-      circles[i].move();
+    }
 
-}
-
-
-    if(score < 1){
-      background(0,random(150,255),0);
+    if(score < 25){
       noStroke();
       fill(250);
       textSize(100);
       textAlign(CENTER);
       text('Conseguiste!', width/2, height/2);
-      text('Conheces os números primos', width/2, height/2+75);
-      noLoop();
     }
 
-    if(hp == true){
-      background(random(150,255),0,0);
-      fill(250);
-      textSize(100);
-      textAlign(CENTER);
-      text('Perdeste!', width/2, height/2);4
-      textSize(40);
-      text('Clica F5 para tentar outra vez', width/2, height/2+75);
-      noLoop();
-    }
+
 
 }
 
 
-function mousePressed() {
-  for (let i = 0; i <circles.length; i++) {
-    if (circles[i].contains(mouseX, mouseY)) {
-      let c = 0;
-      while(c<primos.length){
-        if(circles[i].num == primos[c]){
-          hp = true;
-          control = true;
-        }
-        c++;
-      }
-      circles.splice(i,1);
-      score = score-1;
-      }
 
-  }
-
-}
 
 //  for (let circle of circles) {
 //    circle.show();
@@ -103,13 +72,11 @@ function mousePressed() {
 //  }
 //}
 
-
-
 class circle {
-  constructor(x, y, r, num) {
+  constructor(x, y, d, num) {
     this.x = x;
     this.y = y;
-    this.r = r;
+    this.d = d;
     this.brightness = 0;
     this.num = num;
   }
@@ -118,30 +85,24 @@ class circle {
       this.brightness = bright;
     }
 
-    contains(x, y) {
-
-      let d = dist(mouseX, mouseY, this.x, this.y);
-      if (d < this.r) {
+    contains(px, py) {
+      let r = dist(px, py, this.x, this.y);
+      if (r < this.d) {
         return true;
       } else {
         return false;
-
       }
     }
 
- move() {
-    this.x = this.x + random(-0.75, 0.75);
-    this.y = this.y + random(-0.75, 0.75);
-  }
   show() {
    stroke(255);
    strokeWeight(4);
    fill(this.brightness, 125);
-   ellipse(this.x, this.y, this.r*2 ,this.r*2);
+   ellipse(this.x, this.y, this.d);
    noStroke();
    fill(255);
    textAlign(CENTER, CENTER);
-   textSize(20);
    text(this.num, this.x,this.y);
   }
+
 }
