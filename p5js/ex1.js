118c66ecc8f4063b71e34b15a20f43629d53ae64
faus2ribeiro <Faus2Ribeiro@users.noreply.github.com>
2020-02-07 17:14:46 +0000

    last

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,11 +1,15 @@
 //lista de variáveis
+
 let circles = [];
 let primos = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97];
+
 let score = 75;
 let hp = false;
+
 let control = false;
 
 
+//setup
 function setup(){
 
   createCanvas(windowWidth,windowHeight);
@@ -21,20 +25,22 @@ function setup(){
     circles.push(c);
 
   }
-
 }
 
-
-
-
+//draw
 function draw() {
   background(50);
+
   noStroke();
   fill(250);
   textSize(20);
   textAlign(LEFT);
   text('Retira os números compósitos', 10, 25);
+
   text(score, width-60 ,25);
+
+  textSize(10);
+  text('© Fausto Ribeiro e Matilde Coelho, 2020', width-200, height-10);
 
   for (let i = 0; i < circles.length; i++) {
     if (circles[i].contains(mouseX,mouseY)) {
@@ -44,9 +50,7 @@ function draw() {
     }
       circles[i].show();
       circles[i].move();
-
-}
-
+  }
 
     if(score < 1){
       background(0,random(150,255),0);
@@ -55,6 +59,7 @@ function draw() {
       textSize(100);
       textAlign(CENTER);
       text('Conseguiste!', width/2, height/2);
+      text('Conheces os números primos', width/2, height/2+75);
       noLoop();
     }
 
@@ -71,8 +76,9 @@ function draw() {
 
 }
 
-
+//mouse
 function mousePressed() {
+
   for (let i = 0; i <circles.length; i++) {
     if (circles[i].contains(mouseX, mouseY)) {
       let c = 0;
@@ -80,7 +86,7 @@ function mousePressed() {
         if(circles[i].num == primos[c]){
           hp = true;
           control = true;
-        }
+      }
         c++;
       }
       circles.splice(i,1);
@@ -91,18 +97,7 @@ function mousePressed() {
 
 }
 
-//  for (let circle of circles) {
-//    circle.show();
-//  }
-//
-//  for (let i = 0; i < circles.length; i++) {
-//
-//    circles[i].show();
-//  }
-//}
-
-
-
+//class para um círculo
 class circle {
   constructor(x, y, r, num) {
     this.x = x;
@@ -110,35 +105,43 @@ class circle {
     this.r = r;
     this.brightness = 0;
     this.num = num;
-  }
 
+}
+
+//sobreposição do rato
   changeColor(bright) {
-      this.brightness = bright;
-    }
+    this.brightness = bright;
+}
 
-    contains(x, y) {
+  contains(x, y) {
 
-      let d = dist(mouseX, mouseY, this.x, this.y);
+    let d = dist(mouseX, mouseY, this.x, this.y);
       if (d < this.r) {
         return true;
       } else {
         return false;
 
       }
-    }
+}
 
- move() {
+//velocidade
+  move() {
+
     this.x = this.x + random(-0.75, 0.75);
     this.y = this.y + random(-0.75, 0.75);
-  }
+}
+
+
   show() {
    stroke(255);
    strokeWeight(4);
    fill(this.brightness, 125);
    ellipse(this.x, this.y, this.r*2 ,this.r*2);
+
    noStroke();
    fill(255);
    textAlign(CENTER, CENTER);
+   textSize(20);
    text(this.num, this.x,this.y);
   }
 }
